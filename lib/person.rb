bc6494192cf013ac2f51c903c0f0ac170542c426
ruthwekesa <wekesaruth569@gmail.com>
2022-10-26 17:12:35 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
-  # your code here
+  #your code here
+
+  def initialize(attributes)
+    attributes.each{|key, value| self.class.attr_accessor(key) ; self.send(("#{key}="), value)}
+  end
 end
